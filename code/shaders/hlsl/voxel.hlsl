5c5994a66cd30e7cb97aac24c1d54956ced4d880
uho <uhacz33@gmail.com>
2015-03-24 17:10:30 +0100

    pusta implementacja voxel.hlsl

--- code/shaders/hlsl/voxel.hlsl
+++ code/shaders/hlsl/voxel.hlsl
@@ -19,3 +19,36 @@ shared cbuffer MaterialData: register(b3)
 {
     
 };
+
+struct in_VS
+{
+    uint   instanceID : SV_InstanceID;
+    float4 pos	  	  : POSITION;
+    float3 normal 	  : NORMAL;
+};
+
+struct in_PS
+{
+    float4 h_pos	: SV_Position;
+    float4 s_pos    : TEXCOORD0;
+    float3 w_pos	: TEXCOORD1;
+    float3 w_normal :TEXCOORD2;
+};
+
+struct out_PS
+{  
+    float4 rgba : SV_Target0;
+};
+
+in_PS vs_main( in_VS input )
+{
+    in_PS OUT = (in_PS)0;
+    return OUT;
+}
+
+out_PS ps_main( in_PS input )
+{
+    out_PS OUT;
+    OUT.rgba = float4(0.0, 0.0, 0.0, 1.0);
+    return OUT;
+}
