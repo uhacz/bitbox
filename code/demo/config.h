3278307dc9ccbd087b1ab6775cae59e506bb7c58
uho <uhacz33@gmail.com>
2015-04-19 21:10:28 +0200

    Files for ...config files.

---
+++ code/demo/config.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace bxConfig
+{
+    int global_init( const char* path );
+    void global_deinit();
+
+    const char* global_string( const char* name );
+    int   global_int( const char* name, int defaultValue = -1 );
+    float global_float( const char* name, float defaultValue = 0.f );
+};
