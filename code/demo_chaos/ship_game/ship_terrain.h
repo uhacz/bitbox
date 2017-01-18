3fbdf833e669cf087042fe06b5ae071cab692aa8
uho <uhacz33@gmail.com>
2017-01-18 23:12:57 +0100

    terrain. WIP.

--- code/demo_chaos/ship_game/ship_terrain.h
+++ code/demo_chaos/ship_game/ship_terrain.h
@@ -1,17 +1,26 @@
 #pragma once
 
+#include <util/type.h>
+#include <util/vectormath/vectormath.h>
+
 namespace bx{namespace ship{
 
 struct Terrain
 {
-    u16 _width = 64;
-    u16 _depth = 128;
-    u16 _max_height = 32;
-    u16 _min_height = -16;
+    u32 _num_samples_x = 0;
+    u32 _num_samples_z = 0;
+    u16* _samples = nullptr; // 0 -> 0xFFFF
 
+    u32 _num_tiles_x = 10;
+    u32 _num_tiles_z = 10;
+    f32 _min_height = 0.f;
+    f32 _max_height = 10.f;
+    rdi::RenderSource* _rsources = nullptr;
 
+    void CreateFromFile( const char* filename );
+    void Destroy();
 
-
+    float GetHeightAtPoint( const Vector3 wsPoint );
 };
 
 }}//
