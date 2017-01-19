f40160c8ecaa58e93952e897531f745ab0b1b853
uho <uhacz33@gmail.com>
2017-01-19 22:40:44 +0100

    terrain. WIP.

--- code/demo_chaos/ship_game/ship_terrain.h
+++ code/demo_chaos/ship_game/ship_terrain.h
@@ -10,17 +10,19 @@ struct Terrain
     u32 _num_samples_x = 0;
     u32 _num_samples_z = 0;
     u16* _samples = nullptr; // 0 -> 0xFFFF
+    f32 _sample_scale_xz = 1.f;
+    f32 _sample_scale_y = 1.f;
 
     u32 _num_tiles_x = 10;
     u32 _num_tiles_z = 10;
-    f32 _min_height = 0.f;
-    f32 _max_height = 10.f;
+    
     rdi::RenderSource* _rsources = nullptr;
 
     void CreateFromFile( const char* filename );
     void Destroy();
 
     float GetHeightAtPoint( const Vector3 wsPoint );
+    
 };
 
 }}//
