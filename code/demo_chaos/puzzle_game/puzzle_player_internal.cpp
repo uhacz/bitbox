22b00a42022d2cdfa36f7366f816ca8b47b942eb
uho <uhacz33@gmail.com>
2017-05-30 22:10:53 +0200

    puzzle physics

--- code/demo_chaos/puzzle_game/puzzle_player_internal.cpp
+++ code/demo_chaos/puzzle_game/puzzle_player_internal.cpp
@@ -143,101 +143,117 @@ namespace bx { namespace puzzle {
 
 namespace physics
 {
-    namespace EConst
+namespace EConst
+{
+    enum E
     {
-        enum E
-        {
-            eMAX_BODIES = 64,
-        };
-    }//
-
-    namespace EBody
-    {
-        enum E
-        {
-            eSOFT = 0,
-            eCLOTH,
-            eROPE,
-            _COUNT_,
-        };
-    }//
-
-    struct ParticleManager
-    {
-        u32 capacity;
-        u32 size;
-
-
+        eMAX_BODIES = 64,
     };
+}//
+
+namespace EBody
+{
+    enum E
+    {
+        eSOFT = 0,
+        eCLOTH,
+        eROPE,
+        _COUNT_,
+    };
+}//
+
+struct ParticleManager
+{
+    u32 _capacity = 0;
+    u32 _size = 0;
+
+    static const u32 BAD_ALLOC = UINT32_MAX;
+
+    u32 allocate( u32 amount )
+    {
+        if( _size + amount > _capacity )
+            return BAD_ALLOC;
+
+        const u32 offset = _size;
+        _size += amount;
+
+        return offset;
+    }
+    u32 deallocate( u32 offset, u32 amount )
+    {
+        
+    }
+};
 
 
-    // --- 
-    using Vector3Array = array_t<Vector3F>;
-    using F32Array = array_t<f32>;
-    using U32Array = array_t<u32>;
+// --- 
+using Vector3Array = array_t<Vector3F>;
+using F32Array = array_t<f32>;
+using U32Array = array_t<u32>;
 
     
     
-    // --- constraints
-    struct DistanceC
-    {
-        u32 i0;
-        u32 i1;
-        f32 rl;
-    };
-    struct BendingC
-    {
-        u32 i0;
-        u32 i1;
-        u32 i2;
-        u32 i3;
-        f32 ra;
-    };
-    using DistanceCArray = array_t<DistanceC>;
-    using BendingCArray  = array_t<BendingC>;
+// --- constraints
+struct DistanceC
+{
+    u32 i0;
+    u32 i1;
+    f32 rl;
+};
+struct BendingC
+{
+    u32 i0;
+    u32 i1;
+    u32 i2;
+    u32 i3;
+    f32 ra;
+};
+using DistanceCArray = array_t<DistanceC>;
+using BendingCArray  = array_t<BendingC>;
 
-    // --- bodies
-    struct PhysicsBody
-    {
-        u32 begin = 0;
-        u32 count = 0;
-    };
-    inline bool IsAlive( const PhysicsBody& body ) { return body.count != 0; }
+// --- bodies
+struct PhysicsBody
+{
+    u32 begin = 0;
+    u32 count = 0;
+};
+inline bool IsValid( const PhysicsBody& body ) { return body.count != 0; }
 
-    struct PhysicsSoftBody
-    {
-        PhysicsBody body;
-        u32 rest_pos_begin = 0;
-        u32 rest_pos_count = 0;
+struct PhysicsSoftBody
+{
+    PhysicsBody body;
+    u32 rest_pos_begin = 0;
+    u32 rest_pos_count = 0;
 
-        Vector3Array   rest_p;
-        DistanceCArray distance_c;
-    };
-    struct PhysicsClothBody
-    {
-        PhysicsBody body;
-        DistanceCArray distance_c;
-    };
-    struct PhysicsBodyRope
-    {
-        PhysicsBody body;
-        DistanceCArray distance_c;
-    };
+    Vector3Array   rest_p;
+    DistanceCArray distance_c;
+};
+struct PhysicsClothBody
+{
+    PhysicsBody body;
+    DistanceCArray distance_c;
+};
+struct PhysicsBodyRope
+{
+    PhysicsBody body;
+    DistanceCArray distance_c;
+};
 
-    // --- body id
-    union BodyIdInternal
+// --- body id
+union BodyIdInternal
+{
+    u32 i;
+    struct
     {
-        u32 i;
-        struct
-        {
-            u32 id    : 16;
-            u32 index : 12;
-            u32 type  : 4; // EBody
-        };
+        u32 id    : 16;
+        u32 index : 12;
+        u32 type  : 4; // EBody
     };
-    inline BodyId         ToBodyId        ( BodyIdInternal idi ) { return{ idi.i }; }
-    inline BodyIdInternal ToBodyIdInternal( BodyId id )          { return{ id.i }; }
-    using IdTable = id_table_t< EConst::eMAX_BODIES, BodyIdInternal >;
+};
+inline BodyId         ToBodyId        ( BodyIdInternal idi ) { return{ idi.i }; }
+inline BodyIdInternal ToBodyIdInternal( BodyId id )          { return{ id.i }; }
+using IdTable = id_table_t< EConst::eMAX_BODIES, BodyIdInternal >;
+using PhysicsBodyArray = array_t<PhysicsBody>;
 
 
 // --- solver
@@ -246,15 +262,22 @@ struct Solver
     Vector3Array p0;
     Vector3Array p1;
     Vector3Array v;
-    Vector3Array w;
+    F32Array     w;
         
     IdTable          id_tbl    [EBody::_COUNT_];
     PhysicsSoftBody  soft_body [EConst::eMAX_BODIES];
     PhysicsClothBody cloth_body[EConst::eMAX_BODIES];
+    PhysicsBodyRope  rope_body [EConst::eMAX_BODIES];
+
+    PhysicsBodyArray _to_deallocate;
 
     u32 num_iterations = 4;
     u32 frequency = 60;
     f32 delta_time = 1.f / frequency;
+
+    u32 Capacity() const { return p0.capacity; }
+    u32 Size    () const { return p0.size; }
+
 };
 void ReserveParticles( Solver* solver, u32 count )
 {
@@ -263,6 +286,30 @@ void ReserveParticles( Solver* solver, u32 count )
     array::reserve( solver->v , count );
     array::reserve( solver->w , count );
 }
+
+namespace
+{
+static PhysicsBody AllocateBody( Solver* solver, u32 particleAmount )
+{
+    if( solver->Size() + particleAmount > solver->Capacity() )
+        return{};
+
+    PhysicsBody body;
+    body.begin = solver->Size();
+    body.count = particleAmount;
+
+    for( u32 i = 0; i < particleAmount; ++i )
+    {
+        array::push_back( solver->p0, Vector3F( 0.f ) );
+        array::push_back( solver->p1, Vector3F( 0.f ) );
+        array::push_back( solver->v, Vector3F( 0.f ) );
+        array::push_back( solver->w, 1.f );
+    }
+
+    return body;
+}
+}//
+
 
 void Create( Solver** solver, u32 maxParticles )
 {
@@ -284,27 +331,98 @@ void SetFrequency( Solver* solver, u32 freq )
     solver->delta_time = (f32)( 1.0 / (double)freq );
 }
 
-void Solve( Solver* solver, u32 numIterations, float deltaTime )
+namespace
+{
+static void GarbageCollector( Solver* solver )
+{
+    
+}
+
+static void PredictPositions( Solver* solver, float deltaTime )
 {
 
+}
+
+
+}
+
+void Solve( Solver* solver, u32 numIterations, float deltaTime )
+{
+    GarbageCollector( solver );
 }
 
 BodyId CreateSoftBody( Solver* solver, u32 numParticles )
 {
+    PhysicsBody body = AllocateBody( solver, numParticles );
+    if( !IsValid( body ) )
+        return{ 0 };
 
+    BodyIdInternal idi = id_table::create( solver->id_tbl[EBody::eSOFT] );
+    PhysicsSoftBody& soft = solver->soft_body[idi.index];
+    soft.body = body;
+
+    return ToBodyId( idi );
 }
 
 BodyId CreateCloth( Solver* solver, u32 numParticles )
 {
+    PhysicsBody body = AllocateBody( solver, numParticles );
+    if( !IsValid( body ) )
+        return{ 0 };
 
+    BodyIdInternal idi = id_table::create( solver->id_tbl[EBody::eSOFT] );
+    PhysicsClothBody& cloth = solver->cloth_body[idi.index];
+    cloth.body = body;
+
+    return ToBodyId( idi );
 }
 
 BodyId CreateRope( Solver* solver, u32 numParticles )
 {
+    PhysicsBody body = AllocateBody( solver, numParticles );
+    if( !IsValid( body ) )
+        return{ 0 };
+
     BodyIdInternal idi = id_table::create( solver->id_tbl[EBody::eROPE] );
 
+    PhysicsBodyRope& rope = solver->rope_body[idi.index];
+    rope.body = body;
+
+    return ToBodyId( idi );
 }
+
 void DestroyBody( Solver* solver, BodyId id )
+{
+    BodyIdInternal idi = ToBodyIdInternal( id );
+    SYS_ASSERT( idi.type < EBody::_COUNT_ );
+
+    if( !id_table::has( solver->id_tbl[idi.type], idi ) )
+        return;
+
+    PhysicsBody body = {};
+    switch( idi.type )
+    {
+    case EBody::eSOFT:
+        body = solver->soft_body[idi.index].body;
+        break;
+    case EBody::eCLOTH:
+        body = solver->cloth_body[idi.index].body;
+        break;
+    case EBody::eROPE:
+        body = solver->rope_body[idi.index].body;
+        break;
+    default:
+        SYS_ASSERT( false );
+        break;
+    }
+
+    id_table::destroy( solver->id_tbl[idi.type], idi );
+
+    SYS_ASSERT( IsValid( body ) );
+    array::push_back( solver->_to_deallocate, body );
+}
+
+void SetConstraints( Solver* solver, BodyId id, const ConstraintInfo* constraints, u32 numConstraints )
 {
 
 }
